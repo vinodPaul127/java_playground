a06966721246b1370a52d5c1da0292ccf17efc66
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-06 17:35:36 +0530

    Create 10_whileloop.java

---
+++ 10_whileloop.java
@@ -0,0 +1,14 @@
+{
+	public static void main (String[] args) throws java.lang.Exception
+	 
+	 
+{
+    int i=0;
+    while (i<=10) {
+        System.out.println(i);
+        i++;
+    }
+  
+}
+	   
+}
