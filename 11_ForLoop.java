8e4067e872184caefb4c11eec1c89dae35d0790e
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-06 17:47:08 +0530

    Create 11_ForLoop.java

---
+++ 11_ForLoop.java
@@ -0,0 +1,14 @@
+{
+	public static void main (String[] args) throws java.lang.Exception
+	 
+	 
+{
+    for (char tip='a';tip<='z';tip++){
+      System.out.println(tip);  
+    }
+    for (int i=0;(i<=100);i++){
+    System.out.println(i);
+    }
+    }
+  
+}
