84bf6ce612bba96c15f4794bfccc4d828d9a8308
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-06 17:25:11 +0530

    Create 9_Default.java

---
+++ 9_Default.java
@@ -0,0 +1,21 @@
+{
+	public static void main (String[] args) throws java.lang.Exception
+	 
+	 
+	 {
+	     int day= 2;
+	     switch (day){
+	        
+	         case 6:
+	             System.out.println("Saturday");
+	             break;
+	         case 7:
+	             System.out.println("Sunday");
+	             break;
+	         default:
+	             System.out.println("Waiting For Weekend");
+	             
+	     }
+	 }
+	   
+}
