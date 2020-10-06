b232b04a4113ee8bdb5090d47bfe5b82943404aa
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-06 16:12:16 +0530

    Create 6_Boolean.java

---
+++ 6_Boolean.java
@@ -0,0 +1,22 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	 
+	 
+	 {
+	     int x=7;
+	     int y=29;
+	     System.out.println(y>x);
+	     {
+	         int n=29;
+	         System.out.println(n==27);
+	         {
+	             boolean JavaisFun=true;
+	             boolean JavaisShit=false;
+	             System.out.println(JavaisFun);
+	             System.out.println(JavaisShit);
+	         }
+	     }
+	 }
+	
+}
