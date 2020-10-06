7eb4e93130164cd930f02dc0be93f50a803ae552
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-06 16:40:44 +0530

    Create 7_ifelse.java

---
+++ 7_ifelse.java
@@ -0,0 +1,25 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	 
+	 
+	 {
+	   int i=87;
+	   int j=94;
+	   if (j>i){
+	       System.out.println("j is greater than i");
+	   }
+	   //block 2 started
+	   int age = 38;
+	   if (age >10){
+	     System.out.println("Teens");
+	    } 
+	    else if (age >30)
+	    {
+	         System.out.println("Audult");
+	   }  else
+	   {
+	             System.out.println("Old");
+	       }
+	   
+}}
