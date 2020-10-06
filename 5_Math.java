ecfc5250abaa1593badc2a6a81ab023e3a316072
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-06 16:01:02 +0530

    Create 5_Math.java

---
+++ 5_Math.java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	 
+	 
+	 {
+	     System.out.println(Math.max(9,8));
+	      System.out.println(Math.sqrt(97));
+	 }
+	
+}
