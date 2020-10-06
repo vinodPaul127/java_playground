79976ef112e88fffe6a83bd9b28c855ec5fb0fd0
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-06 18:00:24 +0530

    Create 12_Break.java

---
+++ 12_Break.java
@@ -0,0 +1,16 @@
+{
+	public static void main (String[] args) throws java.lang.Exception{
+	 
+	 
+  
+    
+    for (int i=0;(i<=100);i++){
+        if (i == 49){
+            break;
+        }
+    System.out.println(i);
+    }
+    }
+  
+}
+	  
