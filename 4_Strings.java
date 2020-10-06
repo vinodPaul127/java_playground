7a7fceb2b824ce2138ccb8c3424c4e9ce2cee34d
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-06 15:40:53 +0530

    Create 4_Strings.java

---
+++ 4_Strings.java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 String firstName = "vinod";
+	 String secondName = "paul";
+	 String nickName = (firstName+"  "+secondName);
+	 System.out.println(nickName.toUpperCase());
+	}
+	
+}
