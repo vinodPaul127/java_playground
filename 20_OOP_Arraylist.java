6342f9f8e160c0532e5589ff1db0c4df8785e693
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-07 22:39:36 +0530

    Create 20_OOP_Arraylist.java

---
+++ 20_OOP_Arraylist.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+	}
+}
