00cc2ca2ca05aecfa71b8b4b32bb29daa06fc26f
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-07 23:00:08 +0530

    Create 25_OOP_Wrappar Classes.java

---
+++ 25_OOP_Wrappar Classes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    
+    Integer myInt = 5;
+    Double myDouble = 5.99;
+    Character myChar = 'A';
+    System.out.println(myInt.intValue());
+    System.out.println(myDouble.doubleValue());
+    System.out.println(myChar.charValue());
+	}
+}
