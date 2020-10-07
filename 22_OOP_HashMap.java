d55f082ec8464f8690aee55c042163e4eca40927
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-07 22:50:27 +0530

    Create 22_OOP_HashMap.java

---
+++ 22_OOP_HashMap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+     HashMap<String, String> capitalCities = new HashMap<String, String>();
+    capitalCities.put("England", "London");
+    capitalCities.put("Germany", "Berlin");
+    capitalCities.put("Norway", "Oslo");
+    capitalCities.put("USA", "Washington DC");
+    System.out.println(capitalCities); 
+	}
+}
