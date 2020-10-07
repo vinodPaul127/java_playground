a63ad6dfcc87c6cd9e6ba112d608093749662c4d
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-07 18:21:24 +0530

    Create 17_OOP_Enums.java

---
+++ 17_OOP_Enums.java
@@ -0,0 +1,12 @@
+public class MyClass { 
+  enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+  }
+
+  public static void main(String[] args) { 
+    Level myVar = Level.MEDIUM; 
+    System.out.println(myVar); 
+  } 
+}
