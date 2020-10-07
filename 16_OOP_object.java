d27c55464ee91ed8c8f02a1002c68a68bec418a4
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-07 12:27:26 +0530

    Create 16_OOP_object.java

---
+++ 16_OOP_object.java
@@ -0,0 +1,10 @@
+class Codechef
+{    String txt = "JAVA";
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+    
+    Codechef myObj = new Codechef();
+    System.out.println(myObj.txt);
+  }
+}
