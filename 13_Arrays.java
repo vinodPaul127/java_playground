53541caafae66c961c66a8d527f98057cd0c667c
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-07 08:44:41 +0530

    Create 13_Arrays.java

---
+++ 13_Arrays.java
@@ -0,0 +1,10 @@
+{
+	public static void main (String[] args) throws java.lang.Exception
+	
+	    { 
+	    String [] cars={"ferarri","Nissan","Lamborgini","bugatti"};
+	    for (int i=0;i<cars.length;i++){
+	        System.out.println("Car name is" +"  "+ cars[i]);
+	    }
+	}	 
+}
