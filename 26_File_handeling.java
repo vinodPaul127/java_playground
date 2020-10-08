f84e9c196685364d6ae12bcb905e425d7394492e
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-08 08:51:39 +0530

    Create 26_File_handeling.java

---
+++ 26_File_handeling.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.io.File;  
+import java.io.IOException;
+
+class Codechef {
+  public static void main(String[] args) {
+    try {
+      File myObj = new File("filename.txt");
+      if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getName());
+      } else {
+        System.out.println("File already exists.");
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
