fcff043b8aaef248463cc1cc316201afedaa7627
vinodPaul12 <72431973+vinodPaul127@users.noreply.github.com>
2020-10-08 10:06:17 +0530

    Create 29_Delete_file&folder.java

---
+++ 29_Delete_file&folder.java
@@ -0,0 +1,20 @@
+import java.io.File; 
+
+public class DeleteFolder {
+  public static void main(String[] args) { 
+    File myObj = new File("C:\\Users\\MyName\\Test"); 
+    if (myObj.delete()) { 
+      System.out.println("Deleted the folder: " + myObj.getName());
+    } else {
+      System.out.println("Failed to delete the folder.");
+    } 
+    
+    //my second logic
+      File myObj = new File("filename.txt"); 
+    if (myObj.delete()) { 
+      System.out.println("Deleted the file: " + myObj.getName());
+    } else {
+      System.out.println("Failed to delete the file.");
+    } 
+  } 
+}
